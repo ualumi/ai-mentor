ae3a86b011c67c887c339704c6ebdae1e8b79e93
ualumi <zroblukavy@gmail.com>
2026-04-07 06:14:35 +0300

    update integration api

--- backend/integration_service/app/main.py
+++ backend/integration_service/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Integration Service")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True
